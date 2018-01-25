33d6d66f4683756bad367d7fabe13cbb79a37dda
Bruno Gavarra <bgavarra@gmail.com>
2018-01-25 12:47:18 -0200

    +projeto Angular

---
+++ src/app/mock-alunos.ts
@@ -0,0 +1,14 @@
+import { Aluno } from './aluno';
+
+export const ALUNOS: Aluno[] = [
+  { id: 1, name: 'Goku' },
+  { id: 2, name: 'Kyo' },
+  { id: 3, name: 'Edward Elric' },
+  { id: 4, name: 'Naruto' },
+  { id: 5, name: 'Sasuke' },
+  { id: 6, name: 'Ted Mosby' },
+  { id: 7, name: 'Barney Stinson' },
+  { id: 8, name: 'Floki' },
+  { id: 9, name: 'Ragnar Lothbrok' },
+  { id: 10, name: 'Thorfinn' }
+];
